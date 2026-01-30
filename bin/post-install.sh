08b53dbefadb7b6509f16ddd2a93e36efac936cb
Shoez1 <52619128+Shoez1@users.noreply.github.com>
2026-01-30 16:44:40 -0300

    up

---
+++ bin/post-install.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# Pós-instalação: baixa modelos spaCy e NLTK necessários
+
+set -e
+
+echo "Installing spaCy model..."
+python -m spacy download en_core_web_sm
+
+echo "Downloading NLTK data..."
+python -c "
+import nltk
+nltk.download('punkt')
+nltk.download('stopwords')
+nltk.download('wordnet')
+nltk.download('omw-1.4')
+"
+
+echo "Post-install complete!"
